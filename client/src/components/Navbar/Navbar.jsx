17879a48899d84a23769b16a7494a815f1666f7a
Myself-Ashish-Sarker <myselfashishsarker@gmail.com>
2024-07-26 00:16:54 +0600

    navbar style changed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { IoPersonCircle } from "react-icons/io5";
 
 // main logo
 import mainLogo from "/main_logo/main_logo.png";
@@ -15,6 +16,7 @@ const Navbar = () => {
     </>
     return (
         <div>
+
             <div className="navbar bg-emerald-500">
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -48,9 +50,30 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div className="flex-none">
+                        
+                        <div className="z-50 dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="text-5xl rounded-full">
+                                    {/* <img
+                                        alt="Tailwind CSS Navbar component"
+                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" /> */}
+                                    <IoPersonCircle className="" />
+                                </div>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                <li><Link>Sign In</Link></li>
+                                <li><Link>Dark Mode</Link></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
+
         </div>
     );
 };
