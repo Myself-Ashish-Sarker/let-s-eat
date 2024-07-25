fdc5f465a3a0d710b11f014a5ea82846d3369607
Myself-Ashish-Sarker <myselfashishsarker@gmail.com>
2024-07-25 21:15:24 +0600

    new main logo added for the website, image folder structure changed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// main logo
+import mainLogo from "/main_logo/main_logo.png";
+// main logo
+
 const Navbar = () => {
 
     const links = <>
@@ -34,7 +38,9 @@ const Navbar = () => {
                             {links}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link to="/">
+                        <img className='w-12' src={mainLogo} alt="" />
+                    </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
