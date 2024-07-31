e532ff0d0a2c0c3fe00bb9a3172fd3443b9eae2b
Ashish Sarker <myselfashishsarker@gmail.com>
2024-07-31 19:20:04 +0600

    private route created

--- client/src/router/PrivateRouter.jsx
+++ client/src/router/PrivateRouter.jsx
@@ -1,7 +1,19 @@
 import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate } from "react-router-dom";
 
 const PrivateRouter = ({ children }) => {
-    const { user, loading } = useContext(AuthContext)
+    const { user, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <h1>Wait Please....</h1>
+    }
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate to="/login" />
 };
 
 export default PrivateRouter
